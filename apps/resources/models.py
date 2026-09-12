45f2adc41384f4ad314a8a75acbd531bd64ff939
Marco Moura <marcomoura27052009@gmail.com>
2026-09-12 10:14:39 -0300

    Add: Criacao do model Resource e sua migracao

--- apps/resources/models.py
+++ apps/resources/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Resource(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        DISPONIVEL = 'disponivel', 'Disponível'
+        EM_MANUTENCAO = 'manutencao', 'Em Manutenção'
+
+    nome = models.CharField(max_length=50) 
+    observacoes = models.TextField(max_length=1500, blank=False, null=False)
+    status = models.CharField(
+        max_length=20,
+        choices=StatusChoices.choices,
+        default=StatusChoices.DISPONIVEL,
+    )
+
+    def __str__(self):
+        return f"{self.nome} | ({self.status})"
+
+    class Meta:
+        permissions = [
+            ('can_create_resources', 'Pode criar recursos'),
+            ('can_delete_resources', 'Pode excluir recursos'),
+            ('can_edit_resources', 'Pode editar recursos'),
+        ]
