f70f771676568f010819880bdb83de04685f8ea5
Marco Moura <marcomoura27052009@gmail.com>
2026-09-14 16:31:04 -0300

    feat: adicao de ResourceDeleteView e padrão de URL correspondente

--- apps/resources/urls.py
+++ apps/resources/urls.py
@@ -4,6 +4,7 @@ from .views import (
     ResourcesCreateView,
     ResourceDetailView,
     ResourceUpdateView,
+    ResourceDeleteView,
 )
 
 
@@ -13,24 +14,30 @@ urlpatterns = [
     path(
         '',
         ResourcesListView.as_view(),
-        name='resource_list'
+        name='resource_list',
     ),
 
     path(
         'create/',
         ResourcesCreateView.as_view(),
-        name='resource_create'
+        name='resource_create',
     ),
 
     path(
         'recurso/<str:nome>/',
         ResourceDetailView.as_view(),
-        name='resource_detail'
+        name='resource_detail',
     ),
 
     path(
         'update/<str:nome>',
         ResourceUpdateView.as_view(),
-        name='resource_update'
+        name='resource_update',
     ),
+
+    path(
+        'delete/<str:nome>',
+        ResourceDeleteView.as_view(),
+        name='resource_delete',
+    )
 ]
--- apps/resources/views.py
+++ apps/resources/views.py
@@ -1,7 +1,11 @@
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Resource
 from .forms import ResourceForm
 
@@ -26,5 +30,12 @@ class ResourceUpdateView(UpdateView):
     model = Resource
     form_class = ResourceForm
     success_url = '/recursos/'
+    slug_field = 'nome'
+    slug_url_kwarg = 'nome'
+
+
+class ResourceDeleteView(DeleteView):
+    model = Resource
+    success_url = reverse_lazy('/recursos/')
     slug_field = 'nome'
     slug_url_kwarg = 'nome'
