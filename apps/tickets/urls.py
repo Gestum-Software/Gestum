b3b25551278c9922a43d73771c9ba1b0e506cd86
Marco Moura <marcomoura27052009@gmail.com>
2026-09-14 19:41:21 -0300

    feat: criacao da TicketListView e configuracao no URLConf

---
+++ apps/tickets/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import (
+    TicketListView
+)
+
+
+app_name = 'tickets'
+
+urlpatterns = [
+    path(
+        '',
+        TicketListView.as_view(),
+        name='tickets_list'
+    ),
+]
--- apps/tickets/views.py
+++ apps/tickets/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Ticket
 
-# Create your views here.
+
+class TicketListView(ListView):
+    model = Ticket
