0902d3375718e7ee4f8b904ab185b3ba2eaa0977
Marco Moura <marcomoura27052009@gmail.com>
2026-09-20 11:59:39 -0300

    feat: troca de base.html para dashboard.html

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
 
 
-class BaseTemplateView(TemplateView):
-    template_name = 'core/base.html'
+class DashboardTemplateView(TemplateView):
+    template_name = 'core/dashboard.html'
