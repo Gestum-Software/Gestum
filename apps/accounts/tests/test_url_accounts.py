c38262a9e9e6a38eb456073a623b43d464ad5c63
Marco Moura <marcomoura27052009@gmail.com>
2026-09-21 21:30:40 -0300

    fix: atualiza importações das views para o módulo correto de usuários

--- apps/accounts/tests/test_url_accounts.py
+++ apps/accounts/tests/test_url_accounts.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import resolve, reverse
 
-from apps.accounts.views import (
+from apps.accounts.views.users import (
     AccountsDeleteView,
     AccountsListView,
     AccountsWithGroupsCreateView,
